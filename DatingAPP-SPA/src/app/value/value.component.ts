3a9161b9f4496352332bb38aa82ace53d01df28b
NisanurBulut <nisanurrunasin@hotmail.com>
2019-09-27 19:58:30 +0300

    local bilgisyaarımın port numarası yazıldı

--- DatingAPP-SPA/src/app/value/value.component.ts
+++ DatingAPP-SPA/src/app/value/value.component.ts
@@ -15,7 +15,7 @@ values:any;
     this.getValues();
   }
   getValues(){
-    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+    this.http.get('https://localhost:5001/api/values').subscribe(response => {
     this.values = response;
     }, error => {
       console.log(error);
