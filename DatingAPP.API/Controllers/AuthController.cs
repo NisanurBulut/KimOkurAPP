182748de84d4a2a683a2776c999a76bd6b01fcdc
NisanurBulut <nisanurrunasin@hotmail.com>
2019-09-27 23:11:16 +0300

    *repo kullanarak kullanıcının register olması sağlandı.

--- DatingAPP.API/Controllers/AuthController.cs
+++ DatingAPP.API/Controllers/AuthController.cs
@@ -1,11 +1,13 @@
+using System.Threading.Tasks;
 using DatingAPP.API.Data;
+using DatingAPP.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingAPP.API.Controllers
 {
     [Route("/api/[Controller]")]
     [ApiController]
-    public class AuthController
+    public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _rp;
         public AuthController(IAuthRepository rp)
@@ -13,9 +15,20 @@ namespace DatingAPP.API.Controllers
             _rp = rp;
         }
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string username, string password) {
+        public async Task<IActionResult> Register(string username, string password)
+        {
             //isteği doğrula
-            username=username.ToLower();
-         }
+            username = username.ToLower();
+            if (await _rp.UserExists(username))
+                return BadRequest("Kullanıcı mevcutta vardır.");
+            var userToCreate = new User()
+            {
+                Username = username
+            };
+            var createdUser=await _rp.Register(userToCreate,password);
+            
+            return StatusCode(201);
+        }
     }
 }
+
