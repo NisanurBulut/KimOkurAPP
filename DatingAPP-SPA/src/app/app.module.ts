b79393923dbf1062ad0f120f2b2294d272ec5705
NisanurBulut <nisanurrunasin@hotmail.com>
2019-09-29 21:45:08 +0300

    navbar htmlde ng form oluştuuruldu login işlemi için

--- DatingAPP-SPA/src/app/app.module.ts
+++ DatingAPP-SPA/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
@@ -13,7 +14,8 @@ import { NavComponent } from './nav/nav.component';
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
    providers: [],
    bootstrap: [
--- DatingAPP-SPA/src/app/nav/nav.component.ts
+++ DatingAPP-SPA/src/app/nav/nav.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
+model:any={};
   constructor() { }
 
   ngOnInit() {
   }
-
+login(){
+  console.log(this.model);
+}
 }
