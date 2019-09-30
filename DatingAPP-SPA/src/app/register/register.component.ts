5102f6e920909bf34d25444955b618623d3cee1e
NisanurBulut <nisanurrunasin@hotmail.com>
2019-09-30 21:41:49 +0300

    register html  duzenlendı

--- DatingAPP-SPA/src/app/register/register.component.ts
+++ DatingAPP-SPA/src/app/register/register.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+model:any= {};
   constructor() { }
 
   ngOnInit() {
+  }
+  register(){
+    console.log(this.model);
+  }
+  cancel()
+  {
+    console.log(this.model);
   }
 
 }
