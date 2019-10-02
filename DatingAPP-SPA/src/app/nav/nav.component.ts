0134d1344ed987253d4ece472847d5ce14cf76d9
NisanurBulut <nisanurrunasin@gmail.com>
2019-10-02 15:48:35 +0300

    login ve logouttan sonra router olayı düzenlendi

--- DatingAPP-SPA/src/app/nav/nav.component.ts
+++ DatingAPP-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,8 @@ export class NavComponent implements OnInit {
   model: any = {};
   constructor(
     public authService: AuthService,
-    private alertify: AlertifyService
+    private alertify: AlertifyService,
+    private router: Router
   ) {}
 
   ngOnInit() {}
@@ -23,14 +25,18 @@ export class NavComponent implements OnInit {
       },
       error => {
         this.alertify.error(error);
+      },
+      () => {
+        this.router.navigate(['/members']);
       }
     );
   }
   loggedIn() {
- return this.authService.loggedIn();
+    return this.authService.loggedIn();
   }
   logOut() {
     localStorage.removeItem('token');
     this.alertify.message('oturum kapandi');
+    this.router.navigate(['/home']);
   }
 }
