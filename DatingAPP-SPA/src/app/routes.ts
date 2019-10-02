787ee7ae716e8ea593c8f93e9f446ab4e7874f7e
NisanurBulut <nisanurrunasin@gmail.com>
2019-10-02 15:31:53 +0300

    router calismasina baslandi.

---
+++ DatingAPP-SPA/src/app/routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+
+export const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'members', component: MemberListComponent },
+  { path: 'messages', component: MessagesComponent },
+  { path: 'lists', component: ListsComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
+
