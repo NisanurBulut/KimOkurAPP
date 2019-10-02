433b9d0f9cbf3111282452f93523ac7dd2d77d32
NisanurBulut <nisanurrunasin@gmail.com>
2019-10-02 16:11:08 +0300

    Bütün sayfalarda geçerli olacak authguard tanımlaması yapıldı

--- DatingAPP-SPA/src/app/routes.ts
+++ DatingAPP-SPA/src/app/routes.ts
@@ -3,12 +3,20 @@ import { HomeComponent } from './home/home.component';
 import { MemberListComponent } from './member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
+import { AuthService } from './_services/auth.service';
 
 export const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'members', component: MemberListComponent,canActivate:[authg] },
-  { path: 'messages', component: MessagesComponent },
-  { path: 'lists', component: ListsComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MemberListComponent },
+      { path: 'messages', component: MessagesComponent },
+      { path: 'lists', component: ListsComponent }
+    ]
+  },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
-
