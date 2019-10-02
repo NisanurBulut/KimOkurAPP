fe579121a1030e80f52859407196fa148643bae6
NisanurBulut <nisanurrunasin@gmail.com>
2019-10-02 14:02:00 +0300

    Token bilgidi decode edildi username gösterildi

--- DatingAPP-SPA/src/app/_services/auth.service.ts
+++ DatingAPP-SPA/src/app/_services/auth.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { tokenName } from '@angular/compiler';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
   jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
   constructor(private http: HttpClient) {}
 
@@ -17,6 +19,8 @@ export class AuthService {
         const user = response;
         if (user) {
           localStorage.setItem('token', user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
+          console.log(this.decodedToken);
         }
       })
     );
