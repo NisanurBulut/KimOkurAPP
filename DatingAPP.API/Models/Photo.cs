bce51842a322d22349c55683dae50ec7f1dd9ca6
NisanurBulut <nisanurrunasin@gmail.com>
2019-10-02 16:38:59 +0300

    Userve phptp madeller olusturuldu.

---
+++ DatingAPP.API/Models/Photo.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DatingAPP.API.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; } 
+        public string Description { get; set; } 
+        public DateTime DateAdded { get; set; } 
+        public bool IsMain { get; set; }
+    }
+}
--- DatingAPP.API/Models/User.cs
+++ DatingAPP.API/Models/User.cs
@@ -1,10 +1,26 @@
+using System;
+using System.Collections.Generic;
+
 namespace DatingAPP.API.Models
 {
     public class User
     {
         public int Id { get; set; }
-        public string Username { get; set; }    
+        public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interest { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
     }
-}
