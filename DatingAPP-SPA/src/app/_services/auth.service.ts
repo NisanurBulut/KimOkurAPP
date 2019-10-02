b7e419baf33faeeafb100a12c5e6ac0811a84c27
NisanurBulut <nisanurrunasin@hotmail.com>
2019-10-02 23:07:30 +0300

    baseUrl environment ortamına taşındı

--- DatingAPP-SPA/src/app/_services/auth.service.ts
+++ DatingAPP-SPA/src/app/_services/auth.service.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  baseUrl = 'http://localhost:5000/api/auth/';
+  baseUrl = environment.apiUrl + '/auth/';
   jwtHelper = new JwtHelperService();
   decodedToken: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'login', model).pipe(
