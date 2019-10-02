d99e545ab7a73b7fb1075954b0207f0ca39075e4
NisanurBulut <nisanurrunasin@gmail.com>
2019-10-02 16:40:47 +0300

    DataContext datamodeller  için güncellendi.

--- DatingAPP.API/Data/DataContext.cs
+++ DatingAPP.API/Data/DataContext.cs
@@ -9,5 +9,6 @@ namespace DatingApp.API.Data
         public DataContext(DbContextOptions options):base (options){}
         public DbSet<Value> Values {get;set;}
         public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
     }
 }
