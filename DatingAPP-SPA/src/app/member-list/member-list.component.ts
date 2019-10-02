4154186c77a670c642699f77fab07302c75f74d1
NisanurBulut <nisanurrunasin@hotmail.com>
2019-10-02 23:25:06 +0300

    kullanıcı bilgisi için api isteği yazıldı

--- DatingAPP-SPA/src/app/member-list/member-list.component.ts
+++ DatingAPP-SPA/src/app/member-list/member-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../_models/User';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-member-list',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-
-  constructor() { }
+  users: User[];
+  constructor(private userService: UserService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+    this.loadUsers();
+  }
+  loadUsers() {
+    this.userService.getUsers().subscribe(
+      (users: User[]) => {
+        this.users = users;
+      },
+      error => {
+        this.alertify.error(error);
+      });
   }
 
 }
