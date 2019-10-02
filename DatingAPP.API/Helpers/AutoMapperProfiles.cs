6b5167a7c56267621f99cef3079ec7336536b3cc
NisanurBulut <nisanurrunasin@hotmail.com>
2019-10-02 22:11:03 +0300

    Yas bilgisi hesaplamak icin extensio nyazıldı

--- DatingAPP.API/Helpers/AutoMapperProfiles.cs
+++ DatingAPP.API/Helpers/AutoMapperProfiles.cs
@@ -11,9 +11,15 @@ namespace DatingAPP.API.Helpers
         {
             CreateMap<User, UserListForDto>()
             .ForMember(dest => dest.PhotoUrl, opt =>
-              opt.MapFrom(src => src.Photos.FirstOrDefault().Url));
+                opt.MapFrom(src => src.Photos.FirstOrDefault().Url))
+            .ForMember(dest => dest.Age, opt =>
+                opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+
             CreateMap<User, UserForDetailedDto>().ForMember(dest => dest.PhotoUrl, opt =>
-              opt.MapFrom(src => src.Photos.FirstOrDefault().Url));
+                opt.MapFrom(src => src.Photos.FirstOrDefault().Url))
+            .ForMember(dest => dest.Age, opt =>
+                opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+
             CreateMap<Photo, PhotosForDetailedDto>();
         }
     }
--- DatingAPP.API/Helpers/Extensions.cs
+++ DatingAPP.API/Helpers/Extensions.cs
@@ -1,13 +1,22 @@
+using System;
 using Microsoft.AspNetCore.Http;
 
 namespace DatingAPP.API.Helpers
 {
-    public static class Extensions{
+    public static class Extensions
+    {
         public static void AddApplicationError(this HttpResponse response, string message)
         {
-            response.Headers.Add("Application-Error",message);
-            response.Headers.Add("Access-Control-Expose-Headers","Application-Error");
-            response.Headers.Add("Access-Control-Allow-Origin","*");
+            response.Headers.Add("Application-Error", message);
+            response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+        public static int CalculateAge(this DateTime theDateTime)
+        {
+            var age = DateTime.Today.Year - theDateTime.Year;
+            if (theDateTime.AddYears(age) > DateTime.Today)
+                age--;
+            return age;
         }
     }
 }
