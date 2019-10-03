b7f44f176d843e2d2a2078ae0b626e09a98e84ef
NisanurBulut <nisanurrunasin@gmail.com>
2019-10-03 11:02:54 +0300

    Userdetail  http isteği yazıldı basic html gosterim yapıldı

--- DatingAPP-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingAPP-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/_models/User';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
-
-  constructor() { }
+  user: User;
+  constructor(
+    private userService: UserService,
+    private alertift: AlertifyService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.loadUser();
   }
 
+  //member/4
+  loadUser() {
+    this.userService.getUser(this.route.snapshot.params['id']).subscribe(
+      (user: User) => {
+        this.user = user;
+      },
+      error => {
+        this.alertift.error(error);
+      }
+    );
+  }
 }
--- DatingAPP-SPA/src/app/routes.ts
+++ DatingAPP-SPA/src/app/routes.ts
@@ -4,6 +4,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 
 export const appRoutes: Routes = [
@@ -14,6 +15,7 @@ export const appRoutes: Routes = [
     canActivate: [AuthGuard],
     children: [
       { path: 'members', component: MemberListComponent },
+      { path: 'members/:id', component: MemberDetailComponent },
       { path: 'messages', component: MessagesComponent },
       { path: 'lists', component: ListsComponent }
     ]
