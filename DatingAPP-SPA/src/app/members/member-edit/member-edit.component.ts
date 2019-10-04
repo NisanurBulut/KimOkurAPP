879b89cfef68d03390b904150f456cb5c51a41cb
NisanurBulut <nisanurrunasin@hotmail.com>
2019-10-04 22:20:39 +0300

    put isteği servisten çağrıldı

--- DatingAPP-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingAPP-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -3,6 +3,8 @@ import { User } from 'src/app/_models/User';
 import { ActivatedRoute } from '@angular/router';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -20,7 +22,7 @@ export class MemberEditComponent implements OnInit {
     }
   }
 
-  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService, private userService: UserService, private authService: AuthService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -29,8 +31,13 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateUser() {
-    console.log(this.user);
-    this.alertify.success('Kullanıcı bilgisi başarılı şekilde güncellendi.');
-    this.editForm.reset(this.user);
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user)
+      .subscribe(next => {
+        this.alertify.success('Kullanıcı bilgisi başarılı şekilde güncellendi.');
+        this.editForm.reset(this.user);
+      },
+        error => {
+          this.alertify.error(error);
+        });
   }
 }
