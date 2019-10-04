ac6d779487c6fedeff9527ce75b95f71cfd4c43f
NisanurBulut <nisanurrunasin@hotmail.com>
2019-10-04 22:18:00 +0300

    kısı bılgı guncelleme ıcın gereklı servıs put ıstegı yazıldı

--- DatingAPP-SPA/src/app/_services/user.service.ts
+++ DatingAPP-SPA/src/app/_services/user.service.ts
@@ -20,4 +20,7 @@ export class UserService {
   getUser(id): Observable<User> {
     return this.http.get<User>(this.baseUrl + 'users/' + id);
   }
+  updateUser(id: number, user: User) {
+    return this.http.put(this.baseUrl + 'users/' + id, user);
+  }
 }
