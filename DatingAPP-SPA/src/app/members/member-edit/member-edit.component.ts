b906e7b0755a58815b5e2e5da0e0558d2dd5b28c
NisanurBulut <nisanurrunasin@hotmail.com>
2019-10-04 21:59:57 +0300

    tarayıcı penceresi kapanırken düzenleme işlemi yapılıyyorsa uyarı verir.

--- DatingAPP-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingAPP-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
 import { User } from 'src/app/_models/User';
 import { ActivatedRoute } from '@angular/router';
 import { AlertifyService } from 'src/app/_services/alertify.service';
@@ -12,6 +12,14 @@ import { NgForm } from '@angular/forms';
 export class MemberEditComponent implements OnInit {
   @ViewChild('editForm', { static: true }) editForm: NgForm;
   user: User;
+  //tarayıcı penceresi kapanırken düzenleme işlemi yapılıyyorsa uyarı verir.
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
+
   constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit() {
