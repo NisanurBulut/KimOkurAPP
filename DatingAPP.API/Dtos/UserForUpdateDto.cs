0ae9eae6ef7e9c7aae7dcd7741c68bac5a6f53f6
NisanurBulut <nisanurrunasin@hotmail.com>
2019-10-04 22:03:18 +0300

    kullanıcı guncelleme işlemi için dto yazıldı

---
+++ DatingAPP.API/Dtos/UserForUpdateDto.cs
@@ -0,0 +1,11 @@
+namespace DatingAPP.API.Dtos
+{
+    public class UserForUpdateDto
+    {
+        public int Introduction { get; set; }
+        public int LookinFor { get; set; }
+        public int Interests { get; set; }
+        public int City { get; set; }
+        public int Country { get; set; }
+    }
+}
