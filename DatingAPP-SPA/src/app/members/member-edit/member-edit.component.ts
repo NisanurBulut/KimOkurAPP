96ab2568a49f7a95a48379ae630fb033188db6b8
NisanurBulut <nisanurrunasin@hotmail.com>
2019-10-04 21:10:36 +0300

    kullanıcı bilgi duzenleme sayfasında submit butonu ngforma baglandı

--- DatingAPP-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingAPP-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/_models/User';
 import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -9,11 +10,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberEditComponent implements OnInit {
   user: User;
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
+  }
+
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Kullanıcı bilgisi başarılı şekilde güncellendi.');
   }
 }
