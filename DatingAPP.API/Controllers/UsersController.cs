eac6663a376a143ddf882beed07fa8a995ce3d30
NisanurBulut <nisanurrunasin@hotmail.com>
2019-10-04 22:38:19 +0300

    guncelleme işleminde put isteği id param alır curly yazılması gerekli :( unutulmus

--- DatingAPP.API/Controllers/UsersController.cs
+++ DatingAPP.API/Controllers/UsersController.cs
@@ -36,19 +36,19 @@ namespace DatingAPP.API.Controllers
             var userToReturn = _mapper.Map<UserForDetailedDto>(user);
             return Ok(userToReturn);
         }
-        [HttpPut("id")]
+        
+         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
         {
             if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
-            {
                 return Unauthorized();
-            }
+
             var userFromRepo = await _repo.GetUser(id);
+
             _mapper.Map(userForUpdateDto, userFromRepo);
+
             if (await _repo.SaveAll())
-            {
                 return NoContent();
-            }
             throw new Exception($"{id} nolu kişinin bilgileri güncellenemedi.");
         }
     }
