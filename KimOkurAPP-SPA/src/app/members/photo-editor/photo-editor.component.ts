7706f77b4043327dfe57702035fe2a5d30ec1f9f
NisanurBulut <nisanurrunasin@hotmail.com>
2019-10-06 14:59:19 +0300

    Fotoğraf üzerinde casting işlemi yapıldı yüklemenin ardından htmlde mini galeride goruntulenmesı saglandı

--- KimOkurAPP-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ KimOkurAPP-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -36,7 +36,19 @@ export class PhotoEditorComponent implements OnInit {
     });
 
     this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+    this.uploader.onSuccessItem = (item, response, headers) => {
+      if (response) {
+        const res: Photo = JSON.parse(response);
+
+        const photo = {
+          id: res.id,
+          url: res.url,
+          dateAdded: res.dateAdded,
+          description: res.description,
+          isMain: res.isMain
+        };
+        this.photos.push(photo);
+      }
+    };
   }
-
-
 }
