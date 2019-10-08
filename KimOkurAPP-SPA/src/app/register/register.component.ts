2c96638b242d7393afb0b4cb2828581305fd3a0a
NisanurBulut <nisanurrunasin@hotmail.com>
2019-10-08 21:03:53 +0300

    Formgroup mekanızmasından daha hafıf bır yapı olan formbuilder mekanızmasına geçiş yapıldı.

--- KimOkurAPP-SPA/src/app/register/register.component.ts
+++ KimOkurAPP-SPA/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -13,15 +13,11 @@ export class RegisterComponent implements OnInit {
   model: any = {};
   registerForm: FormGroup;
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService, private fb: FormBuilder) { }
 
 
   ngOnInit() {
-    this.registerForm = new FormGroup({
-      username: new FormControl('', Validators.required),
-      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('', Validators.required)
-    }, this.passwordMatchValidator);
+    this.createRegisterForm();
   }
   passwordMatchValidator(g: FormGroup) {
     return g.get('password').value === g.get('confirmPassword').value ? null : { 'nomatch': true }
@@ -36,8 +32,14 @@ export class RegisterComponent implements OnInit {
     //   }
     // );
   }
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', Validators.required]
+    }, { validator: this.passwordMatchValidator });
+  }
   cancel() {
     this.cancelRegister.emit(false);
-
   }
 }
