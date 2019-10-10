1ccd79187c660ca6e56cd27c9060f58f479f6583
NisanurBulut <nisanurrunasin@hotmail.com>
2019-10-10 21:08:11 +0300

    kullanıcı kayıt işlemi  için api isteği observavle yazıldı

--- KimOkurAPP-SPA/src/app/register/register.component.ts
+++ KimOkurAPP-SPA/src/app/register/register.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { BsDatepickerConfig } from 'ngx-bootstrap';
+import { User } from '../_models/User';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,7 @@ import { BsDatepickerConfig } from 'ngx-bootstrap';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  user: User;//Kaydolacak kullanıcının bilgilerini tutar
   registerForm: FormGroup;
   bsConfig: Partial<BsDatepickerConfig>;
 
@@ -28,14 +29,15 @@ export class RegisterComponent implements OnInit {
     return g.get('password').value === g.get('confirmPassword').value ? null : { 'nomatch': true }
   }
   register() {
-    console.log(this.registerForm.value);
-    // this.authService.register(this.model).subscribe(() => {
-    //   this.alertify.success('kaydolma basarili');
-    // },
-    //   error => {
-    //     this.alertify.error(error);
-    //   }
-    // );
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.authService.register(this.user).subscribe(() => {
+        this.alertify.success('Kullanıcı kayıt işlemi başarılıdır.');
+      },
+        error => {
+          this.alertify.error(error);
+        });
+    }
   }
   createRegisterForm() {
     this.registerForm = this.fb.group({
