18e67c75e7c6f0fb2b46163af5331dac20c7d5ad
NisanurBulut <nisanurrunasin@hotmail.com>
2019-10-10 20:55:08 +0300

    Regisrer işlemi için dto tanımı yapıldı

--- KimOkurAPP.API/Dtos/UserForRegisterDto.cs
+++ KimOkurAPP.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,32 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace KimOkur.API.Dtos
 {
     public class UserForRegisterDto
     {
-    public string Username { get; set; }
-    public string Password { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "Parolanız en az 4 en fazla 8 karakter olmalıdır.")]
+        public string Password { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string KnownAs { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
