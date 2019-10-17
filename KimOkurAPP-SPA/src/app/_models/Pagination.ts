21a8ef44e87af7d828bdeaf7fe5365f2933b10ca
NisanurBulut <nisanurrunasin@gmail.com>
2019-10-17 11:46:27 +0300

    SPA tarafında sayfalama gosterımı için interface ve repo formatta sonuc ınterface yazılmıştır.

---
+++ KimOkurAPP-SPA/src/app/_models/Pagination.ts
@@ -0,0 +1,12 @@
+export interface Pagination {
+  currentPage: number;
+  itemsPerPage: number;
+  totalItems: number;
+  totalPage: number;
+}
+
+export class PaginatedResult<T> {
+  //mesaj listesindede kullanılacak
+  result: T;
+  pagination: Pagination;
+}
