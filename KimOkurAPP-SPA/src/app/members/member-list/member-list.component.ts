c538f2da2221170871d4bd35735d5afe95018edc
NisanurBulut <nisanurrunasin@gmail.com>
2019-10-17 13:52:41 +0300

    Kişiler sayfası için  ngx bootstrap sayfalama component eklentisi gerçekleştirilid.

--- KimOkurAPP-SPA/src/app/members/member-list/member-list.component.ts
+++ KimOkurAPP-SPA/src/app/members/member-list/member-list.component.ts
@@ -3,6 +3,7 @@ import { User } from '../../_models/User';
 import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
+import { Pagination, PaginatedResult } from 'src/app/_models/Pagination';
 
 @Component({
   selector: 'app-member-list',
@@ -11,21 +12,34 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberListComponent implements OnInit {
   users: User[];
-  constructor(private userService: UserService, private alertify: AlertifyService, private route:ActivatedRoute) { }
+  pagination: Pagination;
+  constructor(
+    private userService: UserService,
+    private alertify: AlertifyService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
     });
   }
-  // loadUsers() {
-  //   this.userService.getUsers().subscribe(
-  //     (users: User[]) => {
-  //       this.users = users;
-  //     },
-  //     error => {
-  //       this.alertify.error(error);
-  //     });
-  // }
-
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+  loadUsers() {
+    this.userService
+      .getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+      .subscribe(
+        (res: PaginatedResult<User[]>) => {
+          this.users = res.result;
+          this.pagination = res.pagination;
+        },
+        error => {
+          this.alertify.error(error);
+        }
+      );
+  }
 }
