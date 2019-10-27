0ddddbaa27a4280ff4ce41bc742e9b3044bcf25e
NisanurBulut <nisanurrunasin@hotmail.com>
2019-10-27 20:32:50 +0300

    Like atma işleminde işlemi için http çağrısı tanımlandı.

--- KimOkurAPP-SPA/src/app/members/member-card/member-card.component.ts
+++ KimOkurAPP-SPA/src/app/members/member-card/member-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from 'src/app/_models/User';
+import { AuthService } from 'src/app/_services/auth.service';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-member-card',
@@ -8,9 +11,19 @@ import { User } from 'src/app/_models/User';
 })
 export class MemberCardComponent implements OnInit {
   @Input() user: User;
-  constructor() { }
+  constructor(private authService: AuthService, private userService: UserService,
+              private alertifyService: AlertifyService) { }
 
   ngOnInit() {
   }
+  sendLike(id: number) {
+    return this.userService.sendLike(this.authService.decodedToken.nameid, id)
+      .subscribe(data => {
+        this.alertifyService.success(this.user.knownAs + ' Kullanıcını favoriledin.');
+      },
+        error => {
+          this.alertifyService.error(error);
+        });
 
+  }
 }
