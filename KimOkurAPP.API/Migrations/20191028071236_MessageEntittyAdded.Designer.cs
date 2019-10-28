18ef1f69e3f5c682dccb7ef5dc988264b92e8daf
NisanurBulut <nisanurrunasin@gmail.com>
2019-10-28 10:13:10 +0300

    Message class için migration işlemi yapıldı

--- KimOkurAPP.API/Migrations/20191028071236_MessageEntittyAdded.Designer.cs
+++ KimOkurAPP.API/Migrations/20191028071236_MessageEntittyAdded.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using KimOkur.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingAPP.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20191028071236_MessageEntittyAdded")]
+    partial class MessageEntittyAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
