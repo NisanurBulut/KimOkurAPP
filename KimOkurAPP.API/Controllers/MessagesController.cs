d411aa6a05813970383558b7c9aad6f46e33133d
NisanurBulut <nisanurrunasin@hotmail.com>
2019-10-29 15:40:16 +0300

    gelen kutu kontrol için method yazıldı

--- KimOkurAPP.API/Controllers/MessagesController.cs
+++ KimOkurAPP.API/Controllers/MessagesController.cs
@@ -56,6 +56,15 @@ namespace KimOkurAPP.API.Controllers
 
             return Ok(messages);
         }
+        [HttpGet("thread/{recipientId}")]
+        public async Task<IActionResult> GetMessageThread(int userId, int recipientId)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+            var messageFromRepo=await _repo.GetMessageThread(userId,recipientId);
+            var messageThread=_mapper.Map<IEnumerable<MessageToReturnDto>>(messageFromRepo);
+            return Ok(messageThread);
+        }
         [HttpPost]
         public async Task<IActionResult> CreateMessage(int userId,[FromQuery] MessageForCreationDto messageforCreationDto)
         {
