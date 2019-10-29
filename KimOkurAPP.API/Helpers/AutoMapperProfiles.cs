74b747b211a21f7024d2f16002c0f58853ee2ff8
NisanurBulut <nisanurrunasin@hotmail.com>
2019-10-29 15:16:26 +0300

    Mesajlar get edilirken mesajlasanların profil fotoğraflarının gosterımı için mapper işlemi yapıldı.

--- KimOkurAPP.API/Helpers/AutoMapperProfiles.cs
+++ KimOkurAPP.API/Helpers/AutoMapperProfiles.cs
@@ -31,7 +31,11 @@ namespace KimOkur.API.Helpers
             CreateMap<PhotoForReturnDto,Photo>();
             CreateMap<UserForRegisterDto,User>();
             CreateMap<MessageForCreationDto,Message>().ReverseMap();
-            CreateMap<Message,MessageToReturnDto>();
+            CreateMap<Message,MessageToReturnDto>()
+            .ForMember(m =>m.SenderPhotoUrl,opt=>opt
+                    .MapFrom(u =>u.Sender.Photos.FirstOrDefault(p =>p.IsMain).Url))
+            .ForMember(m =>m.RecipientPhotoUrl,opt=>opt
+                    .MapFrom(u =>u.Recipient.Photos.FirstOrDefault(p =>p.IsMain).Url));
         }
     }
 }
