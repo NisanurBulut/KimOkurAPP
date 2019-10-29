64a06942f592cedf2b28aabc0de9b371159e809c
NisanurBulut <nisanurrunasin@hotmail.com>
2019-10-29 15:36:29 +0300

    Mesaj listeleme işlemi

--- KimOkurAPP.API/Data/DatingRepository.cs
+++ KimOkurAPP.API/Data/DatingRepository.cs
@@ -140,9 +140,16 @@ namespace KimOkur.API.Data
            return await PagedList<Message>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
         }
 
-        public Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
+        public async Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
         {
-            throw new NotImplementedException();
+            var messages=await _dc.Messages
+                .Include(u =>u.Sender).ThenInclude(p =>p.Photos)
+                .Include(u =>u.Recipient).ThenInclude(p =>p.Photos)
+                    .Where(m =>m.RecipientId==userId && m.SenderId==recipientId 
+                    || m.RecipientId==recipientId && m.SenderId==userId)
+                    .OrderByDescending(m =>m.MessageSent)
+                    .ToListAsync();
+                    return messages;
         }
     }
 }
