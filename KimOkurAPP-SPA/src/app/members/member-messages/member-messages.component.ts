3582063227b7dbdebd3ae9caef7e12ce47d29877
NisanurBulut <nisanurrunasin@hotmail.com>
2019-11-02 12:55:13 +0300

    üye detay tabda mesajların goruntulenmesı sağlandı.

--- KimOkurAPP-SPA/src/app/members/member-messages/member-messages.component.ts
+++ KimOkurAPP-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { Message } from 'src/app/_models/Message';
 
 @Component({
   selector: 'app-member-messages',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit {
-
-  constructor() { }
+  @Input() recipientId: number;
+  messages: Message[];
+  newMessage: any = {};
+  constructor(private userService: UserService, private authService: AuthService,
+    private alertify: AlertifyService) { }
 
   ngOnInit() {
+    this.loadMessages();
   }
-
+  loadMessages() {
+    const currentUserId = +this.authService.decodedToken.nameid;
+    this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
+      .subscribe(messages => {
+        this.messages = messages;
+      }, error => {
+        this.alertify.error(error);
+      });
+  }
 }
