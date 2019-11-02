06bf272f6f6e1ce08037ebe6703eb915196124ca
NisanurBulut <nisanurrunasin@hotmail.com>
2019-11-02 12:36:06 +0300

    member-messages component olusturuldu.

---
+++ KimOkurAPP-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-messages',
+  templateUrl: './member-messages.component.html',
+  styleUrls: ['./member-messages.component.css']
+})
+export class MemberMessagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
