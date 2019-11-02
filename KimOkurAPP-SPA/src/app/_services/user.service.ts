5e2558a4968de684f0e9d5bfeea1b08166cf63c9
NisanurBulut <nisanurrunasin@hotmail.com>
2019-11-02 12:35:35 +0300

    profilde mesajların goruntulenebilmesi için http istek hazırlandı.

--- KimOkurAPP-SPA/src/app/_services/user.service.ts
+++ KimOkurAPP-SPA/src/app/_services/user.service.ts
@@ -108,4 +108,8 @@ export class UserService {
         })
       );
   }
+  getMessageThread(id: number, recipientId: number) {
+    return this.http.get<Message[]>(this.baseUrl + 'users/' + id + '/messages/thread/' + recipientId);
+  }
+
 }
