0348ccb690e696ffad0cafd5cb9666e4942b36dc
NisanurBulut <nisanurrunasin@hotmail.com>
2019-11-03 21:38:13 +0300

    Mesajlar butonu ile mesaj tab aktif edildi.

--- KimOkurAPP-SPA/src/app/members/member-detail/member-detail.component.ts
+++ KimOkurAPP-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from 'src/app/_models/User';
 import { UserService } from 'src/app/_services/user.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
@@ -8,6 +8,7 @@ import {
   NgxGalleryImage,
   NgxGalleryAnimation
 } from 'ngx-gallery';
+import { TabsetComponent } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-member-detail',
@@ -15,6 +16,7 @@ import {
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  @ViewChild('memberTabs', { static: true }) memberTabs: TabsetComponent;
   user: User;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
@@ -22,10 +24,10 @@ export class MemberDetailComponent implements OnInit {
     private userService: UserService,
     private alertify: AlertifyService,
     private route: ActivatedRoute
-  ) {}
+  ) { }
 
   ngOnInit() {
-    
+
     this.route.data.subscribe(data => {
       //typescript type sever
       this.user = data['user'];
@@ -40,7 +42,7 @@ export class MemberDetailComponent implements OnInit {
         preview: false
       }
     ];
-    this.galleryImages =this.getImages();
+    this.galleryImages = this.getImages();
   }
   getImages() {
     const imageUrls = [];
@@ -54,7 +56,9 @@ export class MemberDetailComponent implements OnInit {
     }
     return imageUrls;
   }
-
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
+  }
   //member/4
   loadUser() {
     this.userService.getUser(this.route.snapshot.params['id']).subscribe(
